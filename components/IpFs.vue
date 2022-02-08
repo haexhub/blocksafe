efdfe54ff94fc896d30fb2fa85178e222fb851c0
haex <hexxx@ok.de>
2022-02-08 15:35:02 +0100

    erste ipfs config

--- components/IpFs.vue
+++ components/IpFs.vue
@@ -13,6 +13,7 @@
 </template>
 
 <script>
+import { urlSource } from "ipfs-core"
 export default {
   name: "IpfsInfo",
   
@@ -33,8 +34,38 @@ export default {
     async getIpfsNodeInfo() {
       try {
         // Await for ipfs node instance.
-        const ipfs = await this.$ipfs();
+        const ipfs = await this.$ipfs({
+          //repo: "wuluwulu",
+          config: { 
+            //http: "/ip4/127.0.0.1/tcp/5001",
+            Addresse: {
+              "API": "/ip4/127.0.0.1/tcp/5001",
+		"Announce": [],
+		"AppendAnnounce": [],
+		"Gateway": "/ip4/127.0.0.1/tcp/8080",
+		"NoAnnounce": [],
+		"Swarm": [
+			"/ip4/0.0.0.0/tcp/4001",
+			"/ip6/::/tcp/4001",
+			"/ip4/0.0.0.0/udp/4001/quic",
+			"/ip6/::/udp/4001/quic"
+		]
+            },
+
+            "Bootstrap": [
+		"/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+		"/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
+		"/ip4/104.131.131.82/udp/4001/quic/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
+		"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+		"/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+		"/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb"
+	],
+          }
+        });
+        const config = await ipfs.config.getAll()
+        console.log("config", config)
         console.log("ipfs", ipfs)
+        //await ipfs.files.mkdir('/my/beautiful/directory', '-p')
         // Call ipfs `id` method.
         // Returns the identity of the Peer.
         const { agentVersion, id } = await ipfs.id();
@@ -43,6 +74,13 @@ export default {
         // Set successful status text.
         this.status = "Connected to IPFS =)";
         this.online = ipfs.isOnline();
+        const upload = await ipfs.add({
+          path: 'hello.txt',
+    content: 'Hello World 101' 
+    })
+        console.log("upload", upload)
+        const data = await ipfs.cat(upload.path)
+  console.log('Data read back via ipfs.cat:', data)
       } catch (err) {
         // Set error status text.
         this.status = `Error: ${err}`;
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
     <ip-fs />
+
+    <!-- <button @click="addFile">Create Node</button> -->
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import IpFs from '../components/IpFs.vue'
+import { dispatch } from "vuex-pathify"
 
 export default Vue.extend({
-  components: { IpFs },
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  methods: {
+    async createNode () {
+      //await dispatch("web3/createNode")
+      await dispatch("web3/addFile", "hjuhuhu")
+    },
+
+  /*   async addFile () {
+      try { 
+        const node = await this.$ipfs({ silent: true})
+        const { cid } = await node.add('Hello world')
+  console.info(cid)
+      } catch (error) {
+        console.log(error)
+      }
+    }, */
+  }
 })
 </script>
